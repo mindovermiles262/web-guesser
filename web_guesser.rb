c568852f202bf0ea6160c3f64074db6c5377bebf
Erik Johnson <mindovermiles262@gmail.com>
2017-04-24 18:43:55 -0700

    working response with cheater

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,40 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+number = 1 + rand(99)
+
+
 get '/' do
-	number = rand(0..100)
-	erb :index, :locals => {:number => number}
+	guess = params['guess']
+	cheater = params['cheater']
+
+	if cheater == "true"
+		message = "The secret number is #{number} you cheater!"
+	elsif guess != nil
+		guess = guess.to_i
+		message = get_message(guess, number)
+	else
+		message = "Guess a number 1 to 100"	
+	end
+
+	erb :index, :locals => {:message => message}
+end
+
+
+def get_message(guess, number)
+	if guess == number
+		"You guessed it"
+	elsif guess > number # guess is too high
+		if guess - number > 5
+			"Your guess is way too high!"
+		else
+			"Your guess is high"
+		end
+	else # guess is too low
+		if number - guess > 5
+			"Your guess is way too low"
+		else
+			"Your guess is low"
+		end	
+	end	
 end
