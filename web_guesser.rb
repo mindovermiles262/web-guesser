baf4e765f4006a5e612123681cfc0b0a5268c253
Erik Johnson <mindovermiles262@gmail.com>
2017-04-25 21:43:03 -0700

    add colors based on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,35 +4,49 @@ require 'sinatra/reloader'
 number = 1 + rand(99)
 @@guesses_remaining = 11
 
+
 get '/' do
 	guess = params['guess']
 	cheater = params['cheater']
 	message = check_guess(guess, number, cheater)
-	erb :index, :locals => {:message => message, :remaining_message => remaining_message}
+	color = get_color(guess.to_i - number)
+	erb :index, :locals => {:message => message, 
+							:remaining_message => remaining_message,
+							:color => color }
 end
 
 get '/gameover' do
-	"Game Over"
+	erb :gameover
 end
 
 get '/winner' do
-	"You guessed it correctly!"
+	erb :winner, :locals => {:number => number}
 end
 
 
 private
 
 
+def get_color(difference)
+	if difference == 0
+		"green"
+	elsif difference.between?(1,5) || difference.between?(-5,-1)
+		"#ff7f7f"
+	else
+		"red"
+	end
+end
+
+
+
 def remaining_message
+	@@guesses_remaining = @@guesses_remaining -  1
 	"You have #{@@guesses_remaining} guesses remaining!"
 end
 
 
 def check_guess(guess, number, cheater)
-
-	@@guesses_remaining = @@guesses_remaining - 1
-
-	if @@guesses_remaining > 0
+	if @@guesses_remaining > 1
 		if cheater == "true"
 			message = "<h1>The secret number is #{number} you cheater!</h1>"
 		elsif guess != nil
@@ -50,6 +64,7 @@ end
 def get_message(guess, number)
 	if guess == number
 		redirect '/winner'
+		# "Winner!"
 	elsif guess > number # guess is too high
 		if guess - number > 5
 			"#{guess} is way too high!"
